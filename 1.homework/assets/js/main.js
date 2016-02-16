3f4e321d5290213e0af15713475afc25cefc3eab
Dushkov <toni.dushkov@gmail.com>
2016-02-17 01:27:14 +0200

    Upload html and js files

---
+++ 1.homework/assets/js/main.js
@@ -0,0 +1,6 @@
+console.log('Здравей');
+console.log('Tony');
+console.log('Dushkov');
+console.info('Резолюцията на екрана е');
+console.log(window.screen.availHeight);
+console.log(window.screen.availWidth);
