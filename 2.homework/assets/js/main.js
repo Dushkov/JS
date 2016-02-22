866a02a0899ebe5f9c1e6a6e882e257e4c23f8af
Tony <toni.dushkov@gmail.com>
2016-02-22 20:02:01 +0200

    correct js

--- 2.homework/assets/js/main.js
+++ 2.homework/assets/js/main.js
@@ -1,14 +1,12 @@
-
 var shopping_cart = [
 	{ product : 'T-shirt', price : 10 },
 	{ product : 'Jeans', price : 69.9 },
     { product : 'Jacket', price : 119.99 }
 ];
    
-
-
-
-    for (var i = 0; i < shopping_cart.length; i++){
-      console.log(shopping_cart [i]);
-    }
- 
+shopping_cart.forEach ( function ( item ) {
+        item.price = item.price.toFixed(2);
+        var char = 65 - item.product.length - item.price.length; 
+          
+        console.log(item.product + new Array(char).join(' ') + item.price);  
+      });
