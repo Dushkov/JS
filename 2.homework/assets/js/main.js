d4f93369f39677d31b9d0ae39d797906c231e707
Dushkov <toni.dushkov@gmail.com>
2016-02-22 17:00:27 +0200

    Adding js and html file

---
+++ 2.homework/assets/js/main.js
@@ -0,0 +1,14 @@
+
+var shopping_cart = [
+	{ product : 'T-shirt', price : 10 },
+	{ product : 'Jeans', price : 69.9 },
+    { product : 'Jacket', price : 119.99 }
+];
+   
+
+
+
+    for (var i = 0; i < shopping_cart.length; i++){
+      console.log(shopping_cart [i]);
+    }
+ 
