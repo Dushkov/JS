8ff10c4bc38a76b99fa766c9aeb345932164008b
Tony <toni.dushkov@gmail.com>
2016-02-24 12:57:18 +0200

    Решение  на втора задача

--- 3.homework/assets/js/main.js
+++ 3.homework/assets/js/main.js
@@ -1,5 +1,13 @@
-//Откриване на средна аритметична стойнст в масив
+//Работа с масив
+console.log('Работа с масив [5, 78, 34, 1, 54, 99, 7, 22, 11, 47, 83, 67 ]')
 
 var array = [5, 78, 34, 1, 54, 99, 7, 22, 11, 47, 83, 67 ];
-for (var i = 0, totall = 0; i < array.length; totall += array[i++]);
-console.log('Средната аритметична стойнсот е '+ totall/array.length);
+
+
+//Откриване на средната аритметична стойнсот//
+for (var i=0, totall=0; i<array.length; totall += array[i++]);
+console.log('Средната аритметична стойнсот на масива е: '+ totall/array.length);
+
+//Сортиране на масива във възходящ ред//
+array.sort(array);
+console.log('Възходяща подредба: '+array.join(","));
